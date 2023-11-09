7b87c51f2ddf26ebe1ce20826eb9647970dfbf28
Rh Proj <r.proj@hotmail.com>
2023-11-09 17:53:46 +0300

    Partial views

--- Umbr2.Site/Controllers/WSDLServiceController.cs
+++ Umbr2.Site/Controllers/WSDLServiceController.cs
@@ -2,7 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Newtonsoft.Json;
+using StackExchange.Profiling.Internal;
 using Umbr2.Site.Models;
+using Umbr2.Site.Services;
 using Umbraco.Cms.Core.Cache;
 using Umbraco.Cms.Core.Logging;
 using Umbraco.Cms.Core.Routing;
@@ -14,8 +16,8 @@ using Umbraco.Cms.Web.Common.PublishedModels;
 using Umbraco.Cms.Web.Website.Controllers;
 
 namespace Umbr2.Site.Controllers
-{
-    public class WSDLServiceController : SurfaceController
+{//WSDLoginController
+	public class WSDLServiceController : SurfaceController
     {//?
         public WSDLServiceController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory,
                 ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider)
@@ -32,23 +34,31 @@ namespace Umbr2.Site.Controllers
                 //LoginRequest = new LoginRequest();
 
                 LoginResponse response = await techClient.LoginAsync(model.Username, model.Password, "192.0.0.1");//Credential.UserName, Credential.Password, Credential.IPs);  //LoginRequest.UserName, LoginRequest.Password, LoginRequest.IPs);    //("admin","password","localhost");      //("meme", "123", "192.0.0.1");
+                 
+				var customerJson = response?.@return;
 
-                var customerJson = response.@return;
 
-                if (!customerJson.Contains("EntityId"))
-                {
-                    await Console.Out.WriteLineAsync("TryAgain!");
-                    //TempData["failed"] = "Wrong Credentials";
-                    return RedirectToCurrentUmbracoUrl();
-                }
+    //            if (customerJson == null)
+    //            {
+				//	TempData["message"] = "Failed to get login response from the web service";
+				//	return Redirect("/MyAccount");
+				//}
+    //            else if (!customerJson.Contains("EntityId"))
+    //            {
+    //                await Console.Out.WriteLineAsync("TryAgain!");
+    //                TempData["message"] = "Wrong Credentials";
+				//	return Redirect("/MyAccount");
+				//	//return RedirectToCurrentUmbracoUrl();
+				//}
 
-                await Console.Out.WriteLineAsync("Success!");
+                CustomerMemberService.GetMember(customerJson);
+				//TempData["message"] = "Welcome";
+				//var customer = JsonConvert.DeserializeObject<CustomerModel>(customerJson);
 
-                var customer = JsonConvert.DeserializeObject<CustomerModel>(customerJson);
 
-                return Redirect("/MyAccount");
-                //return Redirect("/user-page");
-                //return RedirectToPage("/user-page", customer);
+				//TempData["Customer"] = customerJson;
+				return Redirect("/MyAccount");
+				
             }
             catch (Exception) //паснуть ошибку на страницу
             {
---
+++ Umbr2.Site/Services/CustomerMemberService.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using Umbr2.Site.Models;
+
+namespace Umbr2.Site.Services
+{
+	public static class CustomerMemberService
+	{
+		public static CustomerModel? Customer { get; private set; }
+		//public static bool IsLoggedIn { get; }
+
+        public static void GetMember(string userInfo)
+		{
+			Customer = new();
+			if (userInfo.Contains("EntityId"))
+			{
+				Customer = JsonConvert.DeserializeObject<CustomerModel>(userInfo);
+			}
+		}
+	}
+}
